0296889b92503877e84db391f2ca20811f22be10
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2025-10-28 13:31:37 +0100

    Boucle pour ajouter plusieurs eleves

--- src/BaseEx4Array/ProfessorBaseEx4Array.java
+++ src/BaseEx4Array/ProfessorBaseEx4Array.java
@@ -8,22 +8,31 @@ public class ProfessorBaseEx4Array {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String lastName = getInput(scanner, "Nom de l'élève : ");
-        String firstName = getInput(scanner, "Prénom de l'élève : ");
+        boolean moreStudents = true;
 
-        ArrayList<Double> grades = getGrades(scanner);
+        while (moreStudents) {
 
-        displayStudentInfo(firstName, lastName, grades);
+            String lastName = getInput(scanner, "Nom de l'élève : ");
+            String firstName = getInput(scanner, "Prénom de l'élève : ");
+
+            ArrayList<Double> grades = getGrades(scanner);
+
+            displayStudentInfo(firstName, lastName, grades);
+
+
+            String answer = getInput(scanner, "Voulez-vous saisir un autre élève ? (oui/non) : ");
+            if (answer.equalsIgnoreCase("non")) {
+                moreStudents = false;
+            }
+        }
 
         scanner.close();
     }
-
 
     public static String getInput(Scanner scanner, String message) {
         System.out.print(message);
         return scanner.nextLine();
     }
-
 
     public static ArrayList<Double> getGrades(Scanner scanner) {
         ArrayList<Double> grades = new ArrayList<>();
@@ -48,16 +57,23 @@ public class ProfessorBaseEx4Array {
         return grades;
     }
 
-
     public static void displayStudentInfo(String firstName, String lastName, ArrayList<Double> grades) {
         System.out.println("\nÉlève : " + firstName + " " + lastName);
         System.out.println("Notes : " + grades);
 
         if (!grades.isEmpty()) {
-            double average = BaseEx4Array.getAverage(grades);
+            double average = getAverage(grades);
             System.out.println("Moyenne : " + average);
         } else {
             System.out.println("Aucune note saisie.");
         }
+    }
+
+    public static double getAverage(ArrayList<Double> grades) {
+        double sum = 0;
+        for (double grade : grades) {
+            sum += grade;
+        }
+        return sum / grades.size();
     }
 }
