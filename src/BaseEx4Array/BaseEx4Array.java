29d18d44f3e7dc94ac1ca5bbf4b8db0d5e451764
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2025-10-28 09:42:22 +0100

    Question 4.1 de l'exo 4

--- src/BaseEx4Array/BaseEx4Array.java
+++ src/BaseEx4Array/BaseEx4Array.java
@@ -2,4 +2,44 @@ package BaseEx4Array;
 
 public class BaseEx4Array {
 
+    public static void main(String[] args) {
+
+        double[] notes = {12.5, 8.0, 15.75, 10.0, 18.25, 14.0};
+
+        double min = getMin(notes);
+        double max = getMax(notes);
+        double moyenne = getMoyenne(notes);
+
+        System.out.println("Note la plus petite : " + min);
+        System.out.println("Note la plus grande : " + max);
+        System.out.println("Moyenne des notes  : " + moyenne);
+    }
+
+    public static double getMin(double[] notes) {
+        double min = notes[0];
+        for (double note : notes) {
+            if (note < min) {
+                min = note;
+            }
+        }
+        return min;
+    }
+
+    public static double getMax(double[] notes) {
+        double max = notes[0];
+        for (double note : notes) {
+            if (note > max) {
+                max = note;
+            }
+        }
+        return max;
+    }
+
+    public static double getMoyenne(double[] notes) {
+        double somme = 0;
+        for (double note : notes) {
+            somme += note;
+        }
+        return somme / notes.length;
+    }
 }
